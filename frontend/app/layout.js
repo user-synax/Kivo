e1c4d3196394ce6fac633611d7329a2f42281a18
ayush <usersynax@gmail.com>
2026-08-27 20:39:05 +0530

    Frontend INIT

---
+++ frontend/app/layout.js
@@ -0,0 +1,28 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Kivo - Chat your way",
+  description: "Kivo is a chat application that allows you to communicate with your friends and family in real-time.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ frontend/app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Kivo - Chat your way</h1>
+  );
+}
