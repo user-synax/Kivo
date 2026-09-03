7f0c6b17b52f5c8e4c64165ab00ee9f6bac39531
ayush <usersynax@gmail.com>
2026-09-03 10:07:35 +0530

    New banners

--- frontend/lib/banners.js
+++ frontend/lib/banners.js
@@ -19,5 +19,35 @@ export const BANNER_OPTIONS = [
     id: "pacman-chase",
     label: "Pac-Man Chase banner",
     url: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzltank4dThwdnVjOHgydXN0bmV2eHp6cHFncXBnNzh1dTQ3bmt4NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hkqefnFjn2MWVl6xvq/giphy.gif",
+  },
+  {
+    id: "phoenix-valorant",
+    label: "Phoenix Valorant banner",
+    url: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExanBuOWhnNmZ1OW9zZGtlOGRqejRmaDRzaWh5bmZnb2d6anpiejA5YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Uwfp8HJKRL9kpjjPmU/giphy.gif",
+  },
+  {
+    id: "valorant-chamber",
+    label: "Valorant Chamber banner",
+    url: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWt5ZjBvNmJzbHM0bTdiazhkNGZvMGVlaWw1c2ZwZjVkYm8wcHEydCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/m3N5PpxSyYyFICeSvb/giphy.gif",
+  },
+  {
+    id: "space-waves",
+    label: "Space Waves banner",
+    url: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXh0NXd0NHhycjJ6b212dmYyZnRqdjRnbWZwcjNva2t3MHhjcXR4ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Fbox1ygIqnga5dLinz/giphy.gif",
+  },
+  {
+    id: "minecraft-frog",
+    label: "Minecraft frop banner",
+    url: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGp3eDQzazJjdDM0MzF6M2tleDVkYjZvd2lkYjY3ZjJuNHNjaHp6ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r8GMmlV8qGrfZ3txfX/giphy.gif",
+  },
+  {
+    id: "cosmic-shri-vishnu",
+    label: "Cosmic Shri Vishnu banner",
+    url: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGhham1pb3p4eGs4anp5MXp6NWQwOTVrOHFiaXNtcGdkbDJvc2hkNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/axDCfXOjALXfoxDryF/giphy.gif",
+  },
+    {
+    id: "loog-geometry",
+    label: "Loog Geometry banner",
+    url: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGNhMzZjOWhkemFveG9zZmJsZXkwN2lwY3pyNXRxdjF3dHlnaGNuaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MS6EUbcUsOC6AYRbYf/giphy.gif",
   }
 ];
